d419e74cbad6464d9e17d9e84793519f33afd534
mido-app <yutaka.omido@gmail.com>
2020-04-06 23:11:44 +0900

    initial commit

---
+++ Assets/Scripts/Human.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Human : MonoBehaviour
+{
+    private float velosity = 0.1f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            Debug.Log("Space button pushed!");
+            velosity *= -1.0f;
+        }
+
+        this.transform.position = new Vector2(
+            this.transform.position.x + velosity,
+            this.transform.position.y
+        );
+
+    }
+}
