b8e64fafcdf6d9f2297fe65775172ceb51ec4bf1
Yuki Oshima <39944763+os1ma@users.noreply.github.com>
2020-08-06 22:57:23 +0900

    アニメーションの向きを切り替えながら歩くようアニメーションを追加・スクリプトを更新

--- Assets/Scripts/MainScene/OnKeyPressWalk.cs
+++ Assets/Scripts/MainScene/OnKeyPressWalk.cs
@@ -7,12 +7,15 @@ public class OnKeyPressWalk : MonoBehaviour
 {
 
     float speed = 1;
+    float fixedUpdateTimesPerSeconds = 50;
+
     float vx = 0;
     float vy = 0;
 
     // Start is called before the first frame update
     void Start()
     {
+        // Do nothing
     }
 
     // Update is called once per frame
@@ -20,18 +23,26 @@ public class OnKeyPressWalk : MonoBehaviour
     {
         vx = 0;
         vy = 0;
+
         if (Input.GetKey("right")) {
             vx = speed;
+            this.GetComponent<Animator>().Play("WalkRight");
+
         } else if (Input.GetKey("left")) {
             vx = -1 * speed;            
+            this.GetComponent<Animator>().Play("WalkLeft");
+
         } else if (Input.GetKey("up")) {
             vy = speed;
+            this.GetComponent<Animator>().Play("WalkUp");
+
         } else if (Input.GetKey("down")) {
             vy = -1 * speed;
+            this.GetComponent<Animator>().Play("WalkDown");
         }
     }
 
     void FixedUpdate() {
-        this.transform.Translate(vx / 50, vy / 50, 0);
+        this.transform.Translate(vx / fixedUpdateTimesPerSeconds, vy / fixedUpdateTimesPerSeconds, 0);
     }
 }
