3223677f4b0ddd72a1bf7afb61c0234269329b94
Yuki Oshima <39944763+os1ma@users.noreply.github.com>
2020-09-03 21:31:50 +0900

    ResultScene にトップページに戻るボタンを追加

---
+++ Assets/Scripts/ResultScene/GoTopSceneButton.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GoTopSceneButton : MonoBehaviour
+{
+
+    public void OnClick()
+    {
+        SceneManager.LoadScene("TopScene");
+    }
+
+}
